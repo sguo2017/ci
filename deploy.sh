928515a7cc8265684001fb583f31e452db4eb759
sguo2017 <sguo08@qq.com>
2019-02-24 16:16:08 +0800

    arguments must be process or job IDs

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,8 @@
 
 port=8888
 #根据端口号查询对应的pid
-pid=$(netstat -nlp | grep :$port | awk '{print $7}' | awk -F"/" '{ print $1 }');
+#pid=$(netstat -nlp | grep :$port | awk '{print $7}' | awk -F"/" '{ print $1 }');
+pid=`netstat -nlp | grep :$port | awk '{print $7}' | awk -F"/" '{ print $1 }'`
 
 #杀掉对应的进程，如果pid不存在，则不执行
 if [  -n  "$pid"  ];  then
